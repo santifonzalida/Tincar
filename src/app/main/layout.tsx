cc527e372d23af57a19899040fc3d6dc7a749b09
santifonzalida <santiifonzalida@gmail.com>
2024-09-03 18:26:37 -0300

    fix build

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,5 +1,4 @@
 
-
 export default function Layout ({ children } : { children: React.ReactNode }) {
     return (
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-red-300">
